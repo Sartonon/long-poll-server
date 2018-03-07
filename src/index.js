079a9b09a2fc5f6e1a298e307e208106e36843a5
Santeri Rusila <santtu_1993@hotmail.com>
2018-03-07 10:35:07 +0200

    Changed port number to 3005

--- src/index.js
+++ src/index.js
@@ -35,4 +35,4 @@ router.get('/pastMessages', (req, res) => {
 
 app.use('/longpoll', router);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3005, () => console.log('Example app listening on port 3000!'));
