6615f46a03c510215135edfca9d98b70b59ea858
Santeri Rusila <santtu_1993@hotmail.com>
2018-03-07 10:23:06 +0200

    Long polling server

---
+++ build/main.js
@@ -0,0 +1,145 @@
+require('source-map-support/register')
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const express = __webpack_require__(2);
+const cors = __webpack_require__(3);
+const bodyParser = __webpack_require__(4);
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const messages = [];
+
+const EventEmitter = __webpack_require__(5).EventEmitter;
+const messageBus = new EventEmitter();
+messageBus.setMaxListeners(100);
+
+router.get('/', (req, res) => {
+  res.send('/');
+});
+
+router.get('/messages', (req, res) => {
+  messageBus.once('message', data => {
+    res.json(data);
+  });
+});
+
+router.post('/messages', (req, res) => {
+  console.log(req.body);
+  messages.push(req.body);
+  messageBus.emit('message', req.body);
+  res.status(200).end();
+});
+
+router.get('/pastMessages', (req, res) => {
+  res.send(messages);
+});
+
+app.use('/longpoll', router);
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = require("express");
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("cors");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("body-parser");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("events");
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.map
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const messages = [];
+
+const EventEmitter = require('events').EventEmitter;
+const messageBus = new EventEmitter();
+messageBus.setMaxListeners(100);
+
+router.get('/', (req, res) => {
+  res.send('/');
+});
+
+router.get('/messages', (req, res) => {
+  messageBus.once('message', data => {
+    res.json(data);
+  });
+});
+
+router.post('/messages', (req, res) => {
+  messages.push(req.body);
+  messageBus.emit('message', req.body);
+  res.status(200).end();
+});
+
+router.get('/pastMessages', (req, res) => {
+  res.send(messages);
+});
+
+app.use('/longpoll', router);
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
